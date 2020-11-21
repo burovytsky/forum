b09e4e0cabd7cfa4836bb6d6ecd614e3786d7362
burovytsky <esfero1505@gmail.com>
2020-11-21 09:46:00 +0200

    0. Spring test [#330070]

---
+++ src/test/java/forum/controller/PostControllerTest.java
@@ -0,0 +1,51 @@
+package forum.controller;
+
+
+import forum.Main;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnMessageCreate() throws Exception {
+        this.mockMvc.perform(get("/create"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/create"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnMessageEdit() throws Exception {
+        this.mockMvc.perform(get("/edit?id=2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnMessageTopic() throws Exception {
+        this.mockMvc.perform(get("/topic?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("topic"));
+    }
+}
