e80e17ea3f0f1418ae9b6738fb1115726fa114de
burovytsky <esfero1505@gmail.com>
2020-11-21 11:04:27 +0200

    1. Spring mock [#330069]

--- src/test/java/forum/controller/PostControllerTest.java
+++ src/test/java/forum/controller/PostControllerTest.java
@@ -3,15 +3,24 @@ package forum.controller;
 
 import forum.Main;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import forum.model.Post;
+import forum.service.ForumCrudService;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.verify;
+
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +30,20 @@ class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ForumCrudService service;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnNewPost() throws Exception {
+        this.mockMvc.perform(post("/save").param("name", "new post"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(service).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("new post"));
+    }
 
     @Test
     @WithMockUser
--- src/test/java/forum/controller/RegControllerTest.java
+++ src/test/java/forum/controller/RegControllerTest.java
@@ -1,16 +1,27 @@
 package forum.controller;
 
 import forum.Main;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import forum.model.User;
+import forum.service.ForumCrudService;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +31,26 @@ class RegControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private ForumCrudService service;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnNewUser() throws Exception {
+        this.mockMvc.perform(post("/reg").param("username", "name")
+                .param("password", "1234"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(service).saveUser(argument.capture());
+        assertThat(argument.getValue().getUsername(), is("name"));
+        assertTrue(passwordEncoder.matches("1234", argument.getValue().getPassword()));
+
+    }
 
     @Test
     @WithMockUser
