8f0469c772cbdc587eafcda999effffd02444673
burovytsky <esfero1505@gmail.com>
2020-11-21 12:18:51 +0200

    0. Spring Liquibase [#330031]

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,4 @@
+insert into authorities (authority) values ('ROLE_USER'), ('ROLE_ADMIN');
+
+insert into posts (name, description) values ('О чем этот форум?', 'О чем этот форум. Описание форума....'),
+('Правила форума', 'Правила форумаю правило 1 - описнаие правила, правило 2 - еще описание...');
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -17,6 +17,3 @@ CREATE TABLE users (
                        enabled boolean default true,
                        authority_id int not null references authorities(id)
 );
-
-insert into authorities (authority) values ('ROLE_USER');
-insert into authorities (authority) values ('ROLE_ADMIN');
